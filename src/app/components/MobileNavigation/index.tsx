db279ebbdd27d31c6ed1299ed993aee1817f506f
Harish Iyer <harishiyer@admins-mbp.lan>
2023-10-08 16:36:49 -0400

    pwa changes

--- src/app/components/MobileNavigation/index.tsx
+++ src/app/components/MobileNavigation/index.tsx
@@ -1,6 +1,25 @@
+"use client";
+
 import Link from "next/link";
+import { useState } from "react";
 import { AiFillHome, AiFillSetting } from "react-icons/ai";
 const MobileNavigation = () => {
+  const [showInstallMessage, setinstallMessage] = useState(false);
+
+  // Detects if device is on iOS
+  const isIos = () => {
+    const userAgent = window.navigator.userAgent.toLowerCase();
+    return /iphone|ipad|ipod/.test(userAgent);
+  };
+  // Detects if device is in standalone mode
+  const isInStandaloneMode = () =>
+    "standalone" in window.navigator && window.navigator.standalone;
+
+  // Checks if should display install popup notification:
+  if (isIos() && !isInStandaloneMode()) {
+    setinstallMessage(true);
+  }
+
   return (
     <nav className="mobileNavigation lg:hidden fixed z-50 bottom-2 dark:bg-slate-50 bg-slate-900 w-[96%] left-[2%] rounded-2xl">
       <div className="max-w-md flex mx-auto justify-evenly">
@@ -11,7 +30,10 @@ const MobileNavigation = () => {
           </Link>
         </div>
         <div className="py-3 px-3 w-1/2 text-slate-50 dark:text-slate-900 text-xl">
-          <Link className="flex flex-col items-center gap-1 select-none" href={"/"}>
+          <Link
+            className="flex flex-col items-center gap-1 select-none"
+            href={"/"}
+          >
             <AiFillSetting />
             <span className="text-xs">Settings</span>
           </Link>
