c36f965f4072ceb650c5338d8c7de0996666aa41
Harish Iyer <harishiyer@admins-mbp.lan>
2023-10-08 16:40:03 -0400

    pwa changes

--- src/app/components/MobileNavigation/index.tsx
+++ src/app/components/MobileNavigation/index.tsx
@@ -21,7 +21,7 @@ const MobileNavigation = () => {
   }
 
   return (
-    <nav className="mobileNavigation lg:hidden fixed z-50 bottom-2 dark:bg-slate-50 bg-slate-900 w-[96%] left-[2%] rounded-2xl">
+    <nav className="mobileNavigation lg:hidden fixed z-50 bottom-4 dark:bg-slate-50 bg-slate-900 w-[96%] left-[2%] rounded-2xl">
       <div className="max-w-md flex mx-auto justify-evenly">
         <div className="py-3 px-3 w-1/2 text-slate-200 dark:text-slate-900 text-xl">
           <Link className="flex flex-col items-center gap-1" href={"/"}>
