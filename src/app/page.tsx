1dfedeae99175bd8a7018da581b0d684b895cde8
Harish Iyer <harishiyer@admins-mbp.lan>
2023-10-09 00:02:20 -0400

    loading screen changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { getMantras } from "@/../sanity/sanity-utils";
 import Link from "next/link";
 import SiteTitle from "./components/siteTitle";
 import { AiOutlineRight } from "react-icons/ai";
+import { BsPlusSquareFill } from "react-icons/bs";
 
 export default async function Home() {
   const mantras = await getMantras();
@@ -10,8 +11,8 @@ export default async function Home() {
     <>
       <div className="max-w-5xl mx-4 lg:mx-auto pt-10 pb-20">
         <SiteTitle />
-        <div className="mt-20">
-
+        <div className="mt-10">
+          <p className="text-slate-900 dark:text-slate-50 mb-5">Featured Mantras</p>
           {mantras.map((mantra) => (
             <Link
               href={`/mantras/${mantra.slug}`}
@@ -24,8 +25,17 @@ export default async function Home() {
               </span>
             </Link>
           ))}
+          <Link
+            href={"/"}
+            className="text-sm mb-5 mt-10 flex flex-col items-center dark:text-slate-50 gap-3"
+          >
+            <span className="mx-auto text-4xl">
+              <BsPlusSquareFill />
+            </span>
+            Login or Sign Up to curate your daily mantra.
+          </Link>
         </div>
       </div>
-    </> 
+    </>
   );
 }
